8616175f3fc8e48e3003b075ba01f831e2bc3400
asugar13 <asier.ugarteche@gmail.com>
2020-08-31 14:09:49 -0400

    changed code to make it recursive

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ global.Headers = fetch.Headers;
 
 var app = express();
 
-
 // Start the server
 app.listen(8080);
 console.log('Listening on port 8080');
-
+//simple async function to get the refresh_token
 async function getRefreshToken() {
   var username = "BankinClientId";
   var password = "secret";
@@ -32,9 +31,9 @@ async function getRefreshToken() {
   var response = await fetch(url, otherParam);
   var refresh_token = await response.json();
   return refresh_token;
-
 }
 
+//simple async function to get the access_token from the refresh_token
 async function getAccessToken(refresh_token){
   const url = "http://localhost:3000/token";
 
@@ -51,6 +50,68 @@ async function getAccessToken(refresh_token){
   return access_token;
 }
 
+//recursive function to traverse the different account pages and get unique accounts
+async function recursiveAccounts(access_token, accountArray, accounts){
+  if (accounts["link"]["next"] != null){
+    var url = "http://localhost:3000" + accounts["link"]["next"];
+    var myHeaders = new Headers();
+    myHeaders.set('content-type', "application/json");
+    myHeaders.set('Authorization', 'Bearer ' + access_token);
+    var otherParam = {
+      headers:myHeaders,
+      method:"GET"
+    };
+    var response = await fetch(url, otherParam);
+    var the_accounts = await response.json();
+
+    for (var account of the_accounts["account"]){
+      var alreadyExists = false;
+      for (var item of accountArray){
+        if (item["acc_number"] == account["acc_number"]){
+          alreadyExists = true;
+        }
+      }
+      if (!alreadyExists){
+        accountArray.push({acc_number: account["acc_number"], amount:account["amount"]})
+      }
+    }
+    return recursiveAccounts(access_token, accountArray, the_accounts);
+  }
+  else {
+    return accountArray;
+  }
+}
+//recursive function to traverse the different transaction pages and get unique transactions
+async function recursiveTransactions(access_token, transactions, transactionArray){
+  if (transactions["link"]["next"] != null){
+    var url = "http://localhost:3000" + transactions["link"]["next"];
+    var myHeaders = new Headers();
+    myHeaders.set('content-type', "application/json");
+    myHeaders.set('Authorization', 'Bearer ' + access_token);
+    var otherParam = {
+      headers:myHeaders,
+      method:"GET"
+    };
+    var response = await fetch(url, otherParam);
+    var newtransactions = await response.json();
+    for (var transaction of newtransactions["transactions"]){
+      var alreadyExists = false;
+      for (var item of transactionArray){
+        if (item["label"] == transaction["label"]){
+          alreadyExists = true;
+        }
+      }
+      if (!alreadyExists){
+        transactionArray.push({label: transaction["label"], amount: transaction["amount"], currency:transaction["currency"]});
+      }
+    }
+    return recursiveTransactions(access_token, newtransactions, transactionArray);
+  }
+  else {
+    return transactionArray;
+  }
+}
+
 async function getAccountTransactions(access_token){
   var url = "http://localhost:3000/accounts";
   var myHeaders = new Headers();
@@ -61,23 +122,28 @@ async function getAccountTransactions(access_token){
     method:"GET"
   };
   var response = await fetch(url, otherParam);
-  var accounts = await response.json()
+  var accounts = await response.json();
 
-  var result = [];
+  var initAccounts = [];
   for (var account of accounts["account"]){
     var item = {}
     item["acc_number"] = account["acc_number"];
     item["amount"] = account["amount"];
+    initAccounts.push(item);
+  }
+  var allAccounts = await recursiveAccounts(access_token, initAccounts, accounts);
+
+  for (var account of allAccounts){
     let acc_url =  "http://localhost:3000/accounts/" + account["acc_number"] + "/transactions";
     let acc_response = await fetch(acc_url, otherParam);
-    let transactions = await acc_response.json();
-    item["transactions"] = [];
+    try {var transactions =  await acc_response.json();} catch(err){account["transactions"] = [];};
+    var initTransactions = [];
     for (var transaction of transactions["transactions"]){
-      item["transactions"].push({"label":transaction["label"], "amount":transaction["amount"], "currency":transaction["currency"]});
+      initTransactions.push({"label":transaction["label"], "amount":transaction["amount"], "currency":transaction["currency"]});
     }
-    result.push(item);
+    account["transactions"] = await recursiveTransactions(access_token, transactions, initTransactions);
   }
-  return result;
+  return allAccounts;
 }
 
 async function main() {
